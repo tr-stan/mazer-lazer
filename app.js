df0facaae1162b248b264ac29e865a6506509709
Tristan Bennett <betriistan@gmail.com>
2019-01-07 14:16:05 -0600

    improved click functions to reference specific object prop

--- app.js
+++ app.js
@@ -1,29 +1,36 @@
 class Room {
-	constructor(forward, left, right, bg_img) {
-		this.forward = forward;
-		this.left = left;
-		this.right = right;
-		this.bg_img = bg_img;
-	}
-
+    constructor(forward, left, right, bg_img) {
+        this.forward = forward;
+        this.left = left;
+        this.right = right;
+        this.bg_img = bg_img;
+    }
+    leftClick() {
+        $("#currentRoom").css({ background: `url(${this.left}) center center no-repeat` });
+    }
+    rightClick() {
+        $("#currentRoom").css({ background: `url(${this.right}) center center no-repeat` });
+    }
+    forwardClick() {
+        $("#currentRoom").css({ background: 'url("./death-pit.jpg") center center no-repeat' });
+    }
 }
 // create array to hold all room objects
 let rooms = [];
 
 // create room objects to fill array (rooms in maze)
-let room1 = new Room("room2", "room5", "deathTrap");
+let room1 = new Room("room2", "./dungeon-hall-2.jpg", "./dungeon-intersection.jpg");
 console.log(room1);
 rooms.push(room1);
 console.log(rooms);
 let currentRoom = room1;
 
 $("#left").click(function() {
-	$("#currentRoom").css({background: 'url("./dungeon-hall-2.jpg") center center no-repeat'});
+    room1.leftClick();
 })
 $("#forward").click(function() {
-	console.log(currentRoom.forward);
+    room1.forwardClick();
 })
 $("#right").click(function() {
-	console.log(currentRoom.right);
+    room1.rightClick();
 })
-
