302c0b7932b136031cf9c3003223d1ebf2a1eee9
Tristan Bennett <betriistan@gmail.com>
2019-01-08 11:53:12 -0600

    created rooms, movement logic, added start bg image

--- app.js
+++ app.js
@@ -1,36 +1,64 @@
 class Room {
-    constructor(forward, left, right, bg_img) {
-        this.forward = forward;
-        this.left = left;
-        this.right = right;
-        this.bg_img = bg_img;
-    }
-    leftClick() {
-        $("#currentRoom").css({ background: `url(${this.left}) center center no-repeat` });
-    }
-    rightClick() {
-        $("#currentRoom").css({ background: `url(${this.right}) center center no-repeat` });
+    constructor(name, background) {
+        this.name = name;
+        
     }
     forwardClick() {
-        $("#currentRoom").css({ background: 'url("./death-pit.jpg") center center no-repeat' });
+    	currentRoom = this.forward
+    }
+    leftClick() {
+        currentRoom = this.left
+    }
+    rightClick() {
+    	currentRoom = this.right
     }
 }
-// create array to hold all room objects
-let rooms = [];
 
-// create room objects to fill array (rooms in maze)
-let room1 = new Room("room2", "./dungeon-hall-2.jpg", "./dungeon-intersection.jpg");
-console.log(room1);
-rooms.push(room1);
-console.log(rooms);
-let currentRoom = room1;
 
-$("#left").click(function() {
-    room1.leftClick();
-})
+let start = new Room("start");
+let room2 = new Room("room 2");
+let room3 = new Room("room 3");
+let room4 = new Room("room 4");
+let room5 = new Room("room 5");
+let room6 = new Room("room 6");
+let cipher = new Room("cipher");
+let death = new Room("death");
+let victory = new Room("victory");
+start.forward = room2;
+start.left = room5;
+start.right = death;
+room2.forward = room3;
+room2.left = death;
+room2.right = room5;
+room3.forward = room4;
+room3.left = death;
+room3.right = start;
+room4.forward = death;
+room4.left = cipher;
+room4.right = room2;
+room5.forward = room6;
+room5.left = room3;
+room5.right = cipher;
+room6.forward = cipher;
+room6.left = room5;
+room6.right = death;
+cipher.left = room4;
+cipher.right = room6;
+
+
+let currentRoom = start;
+console.log(currentRoom.name);
+
 $("#forward").click(function() {
-    room1.forwardClick();
+	currentRoom.forwardClick();
+	console.log(`Player clicked forward. Current room is ${currentRoom.name}`);
+})
+$("#left").click(function() {
+	currentRoom.leftClick();
+	console.log(`Player clicked left. Current room is ${currentRoom.name}`);
 })
 $("#right").click(function() {
-    room1.rightClick();
+	currentRoom.rightClick();
+	console.log(`Player clicked right. Current room is ${currentRoom.name}`);
 })
+console.log(`Current room is ${currentRoom.name}`)
