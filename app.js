c00817c082c31be165a0e321b61ae3bb8c73fdbe
Tristan Bennett <betriistan@gmail.com>
2019-01-09 16:01:52 -0600

    added logic to remove clues once clicked

--- app.js
+++ app.js
@@ -27,20 +27,26 @@ let guesses = 3;
 
 // create class for making room objects
 class Room {
-    constructor(name, background, firstClue, secondClue) {
+    constructor(name, background, firstClue, secondClue, firstClicked, secondClicked) {
         this.name = name;
         this.background = background;
         this.firstClue = firstClue;
         this.secondClue = secondClue;
+        this.firstClicked = firstClicked;
+        this.secondClicked = secondClicked;
     }
-
+    // click functions
     forwardClick() {
         currentRoom = this.forward;
         $("#current-room").css({ "background-image": "url(" + this.forward.background + ")" });
-        $("#first").show();
-        $("#first").addClass(this.forward.firstClue);
-        $("#second").show();
-        $("#second").addClass(this.forward.secondClue);
+        if (currentRoom.firstClicked === false) {
+            $("#first").show();
+            $("#first").addClass(this.forward.firstClue);
+        }
+        if (currentRoom.secondClicked === false) {
+            $("#second").show();
+            $("#second").addClass(this.forward.secondClue);
+        }
     }
     leftClick() {
         currentRoom = this.left;
@@ -64,11 +70,11 @@ class Room {
 // create object for each room in maze, connect the rooms according to desired maze logic
 let start = new Room("start", "images/door.jpg")
 let room1 = new Room("room1", "images/spaceship-hall.jpg");
-let room2 = new Room("room 2", "images/two-door-hall.jpg", "good", "bad");
-let room3 = new Room("room 3", "images/space-hall.jpeg", "bad", "good");
-let room4 = new Room("room 4", "images/sci-fi-hall.jpg", "bad", "bad");
-let room5 = new Room("room 5", "images/ship-hall.jpg", "bad", "good");
-let room6 = new Room("room 6", "images/space-hall.jpeg", "good", "bad");
+let room2 = new Room("room 2", "images/two-door-hall.jpg", "good", "bad", false, false);
+let room3 = new Room("room 3", "images/space-hall.jpeg", "bad", "good", false, false);
+let room4 = new Room("room 4", "images/sci-fi-hall.jpg", "bad", "bad", false, false);
+let room5 = new Room("room 5", "images/ship-hall.jpg", "bad", "good", false, false);
+let room6 = new Room("room 6", "images/space-hall.jpeg", "good", "bad", false, false);
 let cipher = new Room("cipher", "images/end-hall.jpg");
 let death = new Room("death", "images/laser-beam.jpg");
 let victory = new Room("victory", "images/utopia.jpg");
@@ -217,6 +223,7 @@ $("#right").click(function() {
 
 // function for each time player clicks on a clue item
 $("#first").click(function() {
+    currentRoom.firstClicked = true;
     if ($(this).hasClass("good")) {
         numClues++;
         $(this).hide();
@@ -229,10 +236,11 @@ $("#first").click(function() {
 })
 
 $("#second").click(function() {
+    currentRoom.secondClicked = true;
     if ($(this).hasClass("good")) {
         numClues++;
         $(this).hide();
-        console.log(`clues: ${numClues}`) 
+        console.log(`clues: ${numClues}`)
     } else {
         lives--;
         $(this).hide();
